4c560ee5e4a600b9a966daedc2f852e616fa68a8
Grégoire Delétang <gdelt@google.com>
2023-04-08 23:17:50 +0100

    Updating agent_process to be more readable, and simplifying some parts of the acting.

--- agent_process.py
+++ agent_process.py
@@ -1,86 +1,106 @@
+"""File defining the processes (actor, learner)."""
+
+import itertools
 from multiprocessing import Process, Queue
-from a2c import A2C
-import numpy as np
-import variables
+import os
 import threading
 import time
 
+import numpy as np
+
+from a2c import A2C
 from env import SnakeEnv
-import os
+import variables
+
+MAX_IDLE_STEPS = 100
 
 
 class AgentProcess(Process):
 
-    def __init__(self, conn, id, n_games):
-        super(AgentProcess, self).__init__()
-        self.conn = conn
-        self.n_games = n_games
-        self.id = id
-        self.msg_queue = []
-        np.random.seed(self.id * 100)
+    def __init__(self, conn, id: int, n_games: int):
+        """Initializes the agent process.
+
+        Args:
+            conn: The connection to receive the messages from the master.
+            id: The ID of the process (determined by the master).
+            n_games: The number of episodes to play until the data is returned.
+                Used by the actors only.
+        """
+        super().__init__()
+        self._conn = conn
+        # Buffer for the input messages from the master. Shouldn't be much used
+        # in practice.
+        self._msg_queue = []
+
+        self._n_games = n_games
+        self._id = id
+        self._env = SnakeEnv()
+
+        # Seed the RNG for the process with 100*id.
+        np.random.seed(self._id * 100)
+
+    def gather_data(self):
+        """Acts in the environment and produce batches of data."""
+        print(f"Process {self._id} starts playing {self._n_games} games.")
+        batch_values = []
+        batch_states = []
+        batch_actions = []
+        scores = []
+        for i in range(self._n_games):
+            state = env.reset()
+            # This variable will allow us to track the last time the agent
+            # scored, to avoid playing infinitely.
+            last_scoring = -1
+            for timestep in itertools.count():
+                action = self._agent([state])
+                new_state, reward, done = env.step(action)
+                if reward == 1:
+                    for j in range(timestep - last_scoring):
+                        batch_values.append(1)
+                    last_scoring = timestep
+
+                batch_states.append([state])
+                batch_actions.append(action)
+                # If the agent hasn't scored in MAX_IDLE_STEPS steps, we reset the
+                # environment.
+                if done or (timestep - last_scoring >= MAX_IDLE_STEPS):
+                    for j in range(timestep - last_scoring - 1):
+                        batch_values.append(0)
+                    break
+                state = new_state
+            scores.append(env.score)
+        print(f"Process {self._id} finished playing.")
+        batch = (batch_states, batch_actions, batch_values)
+        self._conn.send((np.mean(scores), batch))
 
     def run(self):
-        self.agent = A2C(self.id)
+        # First instanciate the object dealing with Tensorflow.
+        self._agent = A2C(self._id)
 
-        def treatQueue():
-            msg = self.conn.recv()
+        def treat_queue():
+            """Deals with the messages sent by the master."""
+            msg = self._conn.recv()
             if msg == "load":
-                self.agent.load_model()
-                print("Process " + str(self.id) + " loaded the master (0) model.")
+                self._agent.load_model()
+                print(
+                    f"Process {self._id} loaded the master (0) model."
+                )
 
             if msg[0] == "train_with_batchs":
+                # This message is only received by the master.
+                assert self._id == 0
                 print("Master process is training ...")
                 t0 = time.time()
-                self.agent.train_with_batchs(msg[1])
-                self.agent.save_model()
+                self._agent.train_with_batchs(msg[1])
+                self._agent.save_model()
+                delta = time.time() - t0
                 print(
-                    "Master process finished training. Time : "
-                    + str(time.time() - t0)
-                    + " \n"
+                    f"Master process finished training. Time : {delta}s \n"
                 )
-                self.conn.send("saved")
+                self._conn.send("saved")
 
         while True:
-            if self.id != 0:
-                batch_values = []
-                batch_states = []
-                batch_actions = []
-                print(
-                    "Process "
-                    + str(self.id)
-                    + " starts playing "
-                    + str(self.n_games)
-                    + " games."
-                )
-                scores = []
-                env = SnakeEnv()
-                overall_data = 0
-                for i in range(self.n_games):
-                    state = env.reset()
-                    t = 0
-                    lastScoring = -1
-                    while True:
-                        action = self.agent([state])
-                        newState, reward, done = env.step(action)
-                        if reward == 1:
-                            for j in range(t - lastScoring):
-                                batch_values.append(1)
-                            lastScoring = t
-
-                        batch_states.append([state])
-                        batch_actions.append(action)
-                        t += 1
-                        if done or (t - lastScoring >= 100):
-                            for j in range(t - lastScoring - 1):
-                                batch_values.append(0)
-                            break
-                        state = newState
-                    scores.append(env.score)
-                    overall_data += t
-
-                    if overall_data >= 10000:
-                        break
-                print("Process " + str(self.id) + " finished playing.")
-                batch = (batch_states, batch_actions, batch_values)
-                self.conn.send((np.mean(scores), batch))
-            treatQueue()
+            if self._id != 0:
+                # Only the actors gather data, not the learner.
+                self.gather_data()
+            treat_queue()
